a7bcb34426a7f32691236c23aba07513df1db2e4
hubaloleg <hubal.oleg@gmail.com>
2016-11-10 19:26:59 +0200

    10.11.2016/final commit

--- app/src/main/java/com/oleg/hubal/showyourgallery/ImageFragment.java
+++ app/src/main/java/com/oleg/hubal/showyourgallery/ImageFragment.java
@@ -1,8 +1,23 @@
 package com.oleg.hubal.showyourgallery;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by User on 10.11.2016.
  */
 
-public class ImageFragment {
+public class ImageFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_image , container, false);
+
+        return v;
+    }
 }
--- app/src/main/java/com/oleg/hubal/showyourgallery/MainActivity.java
+++ app/src/main/java/com/oleg/hubal/showyourgallery/MainActivity.java
@@ -2,14 +2,28 @@ package com.oleg.hubal.showyourgallery;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private FrameLayout mDisplayContainer;
+    private FrameLayout mGalleryContainer;
+
+    private GalleryFragment mGalleryFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mDisplayContainer = (FrameLayout) findViewById(R.id.fl_display_container);
+        mGalleryContainer = (FrameLayout) findViewById(R.id.fl_gallery_container);
 
+        mGalleryFragment = new GalleryFragment();
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fl_gallery_container, mGalleryFragment)
+                .commit();
     }
+
 }
